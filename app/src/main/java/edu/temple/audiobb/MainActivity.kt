dc0823b1f2e0d04b0dae38527b288d1e0813f519
Liam <Liam@DESKTOP-BBOI67G>
2021-10-26 16:01:06 -0400

    Add List of books to MainActivity.kt

--- app/src/main/java/edu/temple/audiobb/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobb/MainActivity.kt
@@ -7,5 +7,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var books:BookList = BookList()
+        books.add(Book("A Game of Thrones","George R. R. Martin"))
+        books.add(Book("The Lightning Thief","Rick Riordan"))
+        books.add(Book("The Ruins of Gorlan","John Flanagan"))
+        books.add(Book("Fahrenheit 451","Ray Bradbury"))
+        books.add(Book("1984","George Orwell"))
+        books.add(Book("The Lord of the Rings","J.R.R. Tolkien"))
+        books.add(Book("Watchmen","Alan Moore"))
+        books.add(Book("The Iliad","Homer"))
+        books.add(Book("Moby-Dick","Herman Melville"))
+        books.add(Book("The Alchemist","Paulo Coelho"))
+
     }
 }
